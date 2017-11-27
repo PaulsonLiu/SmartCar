00ba3e07734f08c1ae84c41022d2007683396811
Paulson.LIU <Paulsonliu@163.com>
2017-11-27 09:30:55 +0800

    集合运动

--- SmartCar.ino
+++ SmartCar.ino
@@ -20,7 +20,6 @@ void setup()
 void loop()
 {
   /* add main program code here */
-
 	keyPressHandler();
 
 }
@@ -46,16 +45,30 @@ void keyPressHandler() {
 				if (isMotorRunning)
 				{
 					digitalWrite(LedPin, HIGH);
-					motorRun(10);
+					//run(10);
+					moveTest();
 				}
 				else
 				{
 					digitalWrite(LedPin, LOW);
-					motorStop();
+					brake(5);
 				}
 			}
 		}
 	}
+}
+
+/*动作调试*/
+void moveTest() {
+	back(10); //后退1s
+	brake(5);//停止0.5s
+	run(10);//前进1s
+	brake(5);//停止0.5s
+	left(10);//向左转1s
+	right(10);//向右转1s
+	spin_right(20); //向右旋转2s
+	spin_left(20);//向左旋转2s
+	brake(5);//停车
 }
 
 /*舵机操作*/
@@ -86,7 +99,7 @@ void servopulse(int servopin, int myangle)/*定义一个脉冲函数，用来模拟方式产生PW
 }
 
 /*电机运动*/
-void motorRun(int time)     // 前进
+void run(int time)     // 前进
 {
 	digitalWrite(Right_motor_go, HIGH);  // 右电机前进
 	digitalWrite(Right_motor_back, LOW);
@@ -98,11 +111,83 @@ void motorRun(int time)     // 前进
 	analogWrite(Left_motor_back, 0);
 	delay(time * 100);   //执行时间，可以调整  
 }
-/*电机停止*/
-void motorStop() {
-	digitalWrite(Right_motor_go, LOW); 
+
+/*电机刹车*/
+void brake(int time)         //刹车，停车
+{
+	digitalWrite(Right_motor_go, LOW);
 	digitalWrite(Right_motor_back, LOW);
 	digitalWrite(Left_motor_go, LOW);
 	digitalWrite(Left_motor_back, LOW);
+	delay(time * 100);//执行时间，可以调整  
 }
 
+/*左转(左轮不动，右轮前进)*/
+void left(int time)
+{
+	digitalWrite(Right_motor_go, HIGH);	// 右电机前进
+	digitalWrite(Right_motor_back, LOW);
+	analogWrite(Right_motor_go, 200);
+	analogWrite(Right_motor_back, 0);//PWM比例0~255调速
+	digitalWrite(Left_motor_go, LOW);   //左轮不动
+	digitalWrite(Left_motor_back, LOW);
+	analogWrite(Left_motor_go, 0);
+	analogWrite(Left_motor_back, 0);//PWM比例0~255调速
+	delay(time * 100);	//执行时间，可以调整  
+}
+
+/*左转(左轮后退，右轮前进)*/
+void spin_left(int time) 
+{
+	digitalWrite(Right_motor_go, HIGH);	// 右电机前进
+	digitalWrite(Right_motor_back, LOW);
+	analogWrite(Right_motor_go, 200);
+	analogWrite(Right_motor_back, 0);//PWM比例0~255调速
+	digitalWrite(Left_motor_go, LOW);   //左轮后退
+	digitalWrite(Left_motor_back, HIGH);
+	analogWrite(Left_motor_go, 0);
+	analogWrite(Left_motor_back, 200);//PWM比例0~255调速
+	delay(time * 100);	//执行时间，可以调整  
+}
+
+/*右转(右轮不动，左轮前进)*/
+void right(int time)        
+{
+	digitalWrite(Right_motor_go, LOW);   //右电机不动
+	digitalWrite(Right_motor_back, LOW);
+	analogWrite(Right_motor_go, 0);
+	analogWrite(Right_motor_back, 0);//PWM比例0~255调速
+	digitalWrite(Left_motor_go, HIGH);//左电机前进
+	digitalWrite(Left_motor_back, LOW);
+	analogWrite(Left_motor_go, 200);
+	analogWrite(Left_motor_back, 0);//PWM比例0~255调速
+	delay(time * 100);	//执行时间，可以调整  
+}
+
+/*右转(右轮后退，左轮前进)*/
+void spin_right(int time)
+{
+	digitalWrite(Right_motor_go, LOW);   //右电机后退
+	digitalWrite(Right_motor_back, HIGH);
+	analogWrite(Right_motor_go, 0);
+	analogWrite(Right_motor_back, 200);//PWM比例0~255调速
+	digitalWrite(Left_motor_go, HIGH);//左电机前进
+	digitalWrite(Left_motor_back, LOW);
+	analogWrite(Left_motor_go, 200);
+	analogWrite(Left_motor_back, 0);//PWM比例0~255调速
+	delay(time * 100);	//执行时间，可以调整  
+}
+
+/*后退*/
+void back(int time)          
+{
+	digitalWrite(Right_motor_go, LOW);  //右轮后退
+	digitalWrite(Right_motor_back, HIGH);
+	analogWrite(Right_motor_go, 0);
+	analogWrite(Right_motor_back, 150);//PWM比例0~255调速
+	digitalWrite(Left_motor_go, LOW);  //左轮后退
+	digitalWrite(Left_motor_back, HIGH);
+	analogWrite(Left_motor_go, 0);
+	analogWrite(Left_motor_back, 150);//PWM比例0~255调速
+	delay(time * 100);     //执行时间，可以调整  
+}
